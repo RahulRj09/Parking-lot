293bf9afe3fc5e9a7280cb08a47ca0780a1f9cb5
RahulRj09 <rahul18@navgurukul.org>
2019-08-27 18:51:50 +0530

    Add. enum class

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -5,11 +5,6 @@ import parkinglot.*;
 public class Client {
     public static void main(String[] args) {
         ParkingLot parkingLot = new ParkingLot();
-        Vehicle bus = new Bus();
-        Vehicle car = new Car();
-        Vehicle bike = new Bike();
-        System.out.println(parkingLot.park(bus));
-        System.out.println(parkingLot.park(car));
-        System.out.println(parkingLot.park(bike));
+        System.out.println(parkingLot.park(ParkableVehicles.Bike));
     }
 }
--- src/main/java/parkinglot/ParkingLot.java
+++ src/main/java/parkinglot/ParkingLot.java
@@ -3,17 +3,16 @@ package parkinglot;
 public class ParkingLot {
     private Spot spot = new Spot();
 
-    public boolean park(Vehicle vehicle) {
-        if(isAvailable(vehicle.getSpotsNeeded())){
-            spot.setOccupiedSpots(vehicle.getSpotsNeeded());
-            return true;
-        }
-       return false;
-    }
-
     private boolean isAvailable(int spotsNeeded) {
         int emptySpots = spot.getSPOTS()-spot.getOccupiedSpots();
         return emptySpots>=spotsNeeded;
     }
 
+    public boolean park(ParkableVehicles vehicle) {
+        if(isAvailable(vehicle.getSpots())){
+            spot.setOccupiedSpots(vehicle.getSpots());
+            return true;
+        }
+        return false;
+    }
 }
