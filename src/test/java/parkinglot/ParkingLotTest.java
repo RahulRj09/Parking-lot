2a3a69a4cec3d6dfde1141e8d76e8389c922a589
RahulRj09 <rahul18@navgurukul.org>
2019-08-28 14:00:57 +0530

    Add. test case

--- src/test/java/parkinglot/ParkingLotTest.java
+++ src/test/java/parkinglot/ParkingLotTest.java
@@ -23,15 +23,34 @@ public class ParkingLotTest {
     @Test
     public void vehicleShouldNotInParkingLot() {
         Car car = new Car("12344");
+        Car car1 = new Car("12344");
         ParkingLot parkingLot = new ParkingLot();
         parkingLot.park(car);
-        Car car1 = new Car("12344");
         assertFalse(parkingLot.unPark(car1));
     }
+
     @Test
     public void parkingLotShouldBeVacant() {
         Car car = new Car("12344");
         ParkingLot parkingLot = new ParkingLot();
         assertFalse(parkingLot.unPark(car));
     }
+
+    @Test
+    public void parkingLotShouldBeFull(){
+        Bus bus = new Bus("12344");
+        Bus bus1 = new Bus("12344");
+        Bus bus2 = new Bus("12344");
+        Bus bus3 = new Bus("12344");
+        Bus bus4 = new Bus("12344");
+        Car car = new Car("123445");
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.park(bus);
+        parkingLot.park(bus1);
+        parkingLot.park(bus2);
+        parkingLot.park(bus3);
+        parkingLot.park(bus4);
+        assertFalse(parkingLot.park(car));
+    }
+
 }
