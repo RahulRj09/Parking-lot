d35065d11e44f1fe3aebb9ea68162732eea643b8
RahulRj09 <rahul18@navgurukul.org>
2019-08-28 12:12:02 +0530

    Add. test case

--- src/test/java/parkinglot/ParkingLotTest.java
+++ src/test/java/parkinglot/ParkingLotTest.java
@@ -1,4 +1,14 @@
 package parkinglot;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class ParkingLotTest {
+    @Test
+    public void vehicleShouldBePark() {
+        Car car = new Car("12344");
+        ParkingLot parkingLot = new ParkingLot();
+        assertEquals(true, parkingLot.park(car));
+    }
 }
